2e0fd3744b901526d34a0be9b63c8c70f164b88a
Andre <andrep@tlu.ee>
2018-02-23 21:58:35 +0200

    Initial server.js file

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const morgan = require('morgan')
+//const mongoose = require('mongoose')
+const bodyParse = require('body-parser')
+
+require('dotenv').config()
+
+app.use(morgan('dev'))
+app.use(bodyParse.json())
+app.use(bodyParse.urlencoded({ extended: false}))
+
+app.use((req, res, next) =>{
+    res.status(err.status || 500).json({
+        status: err.status || 500,
+        message: process.env.NODE_ENV === 'development' ? err.message:''
+    })
+})
+
+const port = process.env.APP_PORT || 8080
+app.listen(port, ()=>{
+    console.log('Backend started in '+ process.env.NODE_ENV + ' on port '+ port +'.')
+})
