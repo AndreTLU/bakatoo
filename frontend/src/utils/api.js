b8afcbaa7ad698f889734b6d1512d6c0335e2f37
Andre <andrep@tlu.ee>
2018-03-08 13:48:09 +0200

    API file

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const BASE_URL = '/api'
+
+export default (method, url, query) => {
+    const token = localStorage.jwt
+    return axios
+        .request({
+            method: method,
+            baseURL: BASE_URL,
+            url: url,
+            data: query.data || {},
+            params: query.params || {},
+            headers: token ? { Authorization: `Bearer ${token}`} : {}
+        })
+        .then(response => {
+            return Promise.resolve(response.data)
+        })
+        .catch(err => {
+            console.error(err)
+            return Promise.reject(err.response)
+        })
+}
