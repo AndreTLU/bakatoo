2457e5760b43166c682006d5dfcc1e8db54cb39e
Andre <andrep@tlu.ee>
2018-03-08 13:50:10 +0200

    Server.js clean up and Passport for App.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParse = require('body-parser')
+const expressValidator = require('express-validator')
 const passport = require('passport')
 const helmet = require('helmet')
 const app = express();
-const routes = require('./routes')(express)
+
+const GithubStrategy = require('passport-github2').Strategy
 
 app.use(morgan('dev'))
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({ extended: true}))
 
 app.use(passport.initialize())
-passport.use(new GitHubStrategy({
+passport.use(new GithubStrategy({
     clientID: process.env.GitclientID,
 	clientSecret: process.env.GitClientSecret,
 	callbackURL: process.env.GitCallback
@@ -19,18 +21,18 @@ passport.use(new GitHubStrategy({
     return done(null, profile)
 }))
 
-app.use((req,res,next)=>{
-    const err = new Error('Resource not found')
-    err.status = 404
-    next(err)
-})
-app.use((err, req, res) =>{
-    res.status(err.status || 500).json({
-        status: err.status || 500,
-        message: process.env.NODE_ENV === 'development' ? err.message:''
-    })
-})
+// app.use((req,res,next)=>{
+//     const err = new Error('Resource not found')
+//     err.status = 404
+//     next(err)
+// })
+// app.use((err, req, res) =>{
+//     res.status(err.status || 500).json({
+//         status: err.status || 500,
+//         message: process.env.NODE_ENV === 'development' ? err.message:''
+//     })
+// })
 
-app.use('/api', routes)
+app.use('/api', require('./routes'))
 
 module.exports = app
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,8 @@
-const express = require('express')
-const app = express();
-const morgan = require('morgan')
-//const mongoose = require('mongoose')
-const bodyParse = require('body-parser')
-
-const routes = require('./routes')(express)
 
 require('dotenv').config()
 
-app.use(morgan('dev'))
-app.use(bodyParse.json())
-app.use(bodyParse.urlencoded({ extended: false}))
 
-app.use((req,res,next)=>{
-    const err = new Error('Resource not found')
-    err.status = 404
-    next(err)
-})
-app.use((err, req, res) =>{
-    res.status(err.status || 500).json({
-        status: err.status || 500,
-        message: process.env.NODE_ENV === 'development' ? err.message:''
-    })
-})
-
-app.use('/api', routes)
-
+const app = require('./app')
 const port = process.env.APP_PORT || 8080
 app.listen(port, ()=>{
     console.log('Backend started in '+ process.env.NODE_ENV + ' on port '+ port +'.')
