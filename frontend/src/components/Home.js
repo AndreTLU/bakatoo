49438483be949e263665f5ef27332a5c62f37df7
Andre <andrep@tlu.ee>
2018-03-08 13:51:13 +0200

    Github login button for Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Button } from 'antd'
 
 class Home extends React.Component {
+    constructor(props){
+        super(props)
+        this.startGithubLogin = this.startGithubLogin.bind(this)
+    }
+
+    componentWillMount(){
+        console.log(this.props)
+    }
+
+    startGithubLogin(){
+        const url = window.location.origin + '/api/auth/github'
+        window.open(url, 'name', 'height=600,width=450')
+    }
 
     render(){
         return(
             <div>
                 <h1>Githubi põhine õpikeskkond</h1>
+                <Button htmlType='submit' onClick={this.startGithubLogin}>Github Login</Button>
             </div>
         )
     }
