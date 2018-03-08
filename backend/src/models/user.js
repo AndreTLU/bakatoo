21114b206026eee6bfe444fab224393db2bee289
Andre <andrep@tlu.ee>
2018-03-08 13:47:07 +0200

    User Mongoose Schema

---
+++ backend/src/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema(
+    {
+        profile: {
+            name: { type: String },
+            loginName: { type: String, require: true},
+            email: { type: String }
+        },
+        login: {
+            githubId: { type: String }
+        }
+    },
+    { timestamps: true }
+)
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
