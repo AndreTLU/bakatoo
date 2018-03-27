55718ac5116c99e67e881c7f66427ce03d1dd698
Andre <andrep@tlu.ee>
2018-03-27 08:16:19 +0300

    Server side Axiom

---
+++ backend/src/utils/api.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+
+const BASE_URL = '/api'
+
+module.exports.Api = (method, url, query, header) => {
+    return axios
+        .request({
+            method: method,
+            baseURL: BASE_URL,
+            url: url,
+            data: query.data || {},
+            params: query.params || {},
+            headers: header
+        })
+        .then(response => {
+            return Promise.resolve(response.data)
+        })
+        .catch(err => {
+            return Promise.reject(err.response)
+        })
+}
