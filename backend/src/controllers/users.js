b18af0401fbcd35a83161a3aedfb1197a52ae6d5
Andre <andrep@tlu.ee>
2018-04-04 01:06:27 +0300

    User token blacklisting

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -19,8 +19,25 @@ module.exports.getUser = async (req, res) =>{
             login: {
                 githhubId: user.login.githubId
             },
+            accessToken: user.accessToken,
             updatedAt: user.updatedAt
         }
+    }
+    // update token if more than X seconds from last token update
+    // time to send update token (1h)
+    let secondsFromtoUpdate =
+    req.user.iat + parseInt(process.env.TOKEN_UPDATE_IN_SECONDS)
+    let currentTimestampInSecons = Date.now() / 1000
+    let updateToken = secondsFromtoUpdate <= currentTimestampInSecons
+
+    if (updateToken) {
+    // save prev revoked token
+    const blacklisted = await blacklistToken(req.user)
+    if (!blacklisted) throw new Error('Unable to blacklist active token')
+
+    console.log(`${req.user._id} token blacklisted`)
+    console.log(`sending updated token to ${req.user._id}`)
+    data.token = signToken(user)
     }
     return res.json(data)
 }
