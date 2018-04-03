5a388f718796fb04279db102087e43d1557e9d5c
Andre <andrep@tlu.ee>
2018-04-04 01:11:27 +0300

    Home Container & app route update

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,17 +5,31 @@ import { Provider } from 'react-redux'
 
 import store from './store'
 
+import './styles/main.scss'
+
 import HomeContainer from './containers/HomeContainer'
 import RouteContainer from './containers/RouteContainer'
+import HeaderWrapContainer from './containers/HeaderContainer'
+import SubjectContainer from './containers/SubjectContainer'
+
+import { Layout, LocaleProvider } from 'antd'
+const { Content, Footer } = Layout
 
 render (
     <Provider store={store}>
         <BrowserRouter history={browserHistory}>
-            <div id='content-wrapper'>
-                <Switch>
-                    <Route exact path='/' component={RouteContainer(HomeContainer)}/>
-                </Switch>
-            </div>
+            <Layout>
+                <Route component={HeaderWrapContainer}/>
+                <Content>
+                    <div id='content-wrapper'>
+                        <Switch>
+                            <Route exact path='/' component={RouteContainer(HomeContainer)}/>
+                            <Route path='/subject/:slug' component={RouteContainer(SubjectContainer)} />
+                            <Route path='/subject/:slug/:assign' component={RouteContainer(SubjectContainer)} />
+                        </Switch>
+                    </div>
+                </Content>
+            </Layout>
         </BrowserRouter>
     </Provider>,
     document.querySelector('#main')
--- frontend/src/containers/HomeContainer.js
+++ frontend/src/containers/HomeContainer.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux'
 import Home from '../components/Home'
 import { logout } from '../actions/AuthActions'
+import { getSubjects } from '../actions/HomeActions'
 
 const mapStateToProps = state => ({
     auth: state.auth,
     home: state.home
 })
 
-export default connect(mapStateToProps, { logout })(Home)
+export default connect(mapStateToProps, { getSubjects })(Home)
