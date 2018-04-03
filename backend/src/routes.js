48f650f2158630c9d1b64db98666eaeda31946ac
Andre <andrep@tlu.ee>
2018-04-04 01:05:04 +0300

    Repo, orgs & assignement routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,13 +8,19 @@ const orgs = require('./controllers/organizations')
 
 const { jwtEnsure, jwtCheck } = require('./utils/jwt')
 
-router.get('/auth/github', passport.authenticate('github', { scope: ['user', 'read:org']}))
+router.get('/auth/github', passport.authenticate('github', { scope: ['user', 'admin:org', 'repo']}))
 router.get('/auth/github/oauth2callback', passport.authenticate('github', {failureRedirect: '/', session: false}), asyncMiddleware(auth.githubLogin))
 router.post('/auth/logout', jwtEnsure, asyncMiddleware(auth.logout))
 
 router.get('/users/me', jwtCheck, jwtEnsure, asyncMiddleware(users.getUser))
 router.get('/users', jwtCheck, jwtEnsure, asyncMiddleware(users.getUsers))
 
-router.get('/orgs/:name', jwtCheck, jwtEnsure, asyncMiddleware(orgs.getOrgs))
-router.post('/orgs', jwtCheck, jwtEnsure, asyncMiddleware(orgs.saveOrg))
+router.get('/repos/:slug', jwtCheck, jwtEnsure, asyncMiddleware(orgs.getRepos))
+router.get('/orgs/:slug', jwtCheck, jwtEnsure, asyncMiddleware(orgs.getOrgName))
+
+router.get('/orgs/', jwtCheck, jwtEnsure, asyncMiddleware(orgs.getOrgs))
+
+router.post('/assignements/', jwtCheck, jwtEnsure, asyncMiddleware(orgs.saveAssignement))
+router.get('/assignements/:slug', jwtCheck, jwtEnsure, asyncMiddleware(orgs.getAssignements))
+//router.post('/orgs', jwtCheck, jwtEnsure, asyncMiddleware(orgs.saveOrg))
 module.exports = router
